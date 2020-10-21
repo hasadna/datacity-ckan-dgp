2d56176315ed8c846e361e6839b11795ed34483a
Ori Hoch <ori@uumpa.com>
2020-10-21 19:09:02 +0300

    fix entrypoint

--- env.sh
+++ env.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-[ "${PUBLIC_KEY_B64}" != "" ] && export PUBLIC_KEY="$(echo "${PUBLIC_KEY_B64}" | base64 -d)"
-[ "${PRIVATE_KEY_B64}" != "" ] && export PRIVATE_KEY="$(echo "${PRIVATE_KEY_B64}" | base64 -d)"
+if [ "${PUBLIC_KEY_B64}" != "" ]; then
+  export PUBLIC_KEY="$(echo "${PUBLIC_KEY_B64}" | base64 -d)"
+fi &&\
+if [ "${PRIVATE_KEY_B64}" != "" ]; then
+  export PRIVATE_KEY="$(echo "${PRIVATE_KEY_B64}" | base64 -d)"
+fi
