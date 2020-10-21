4e45c4e27e7b266e8c0ca34a32ad4cba494d591b
Ori Hoch <ori@uumpa.com>
2020-10-21 18:59:20 +0300

    wait for DB before starting container

--- datacity_ckan_entrypoint.sh
+++ datacity_ckan_entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-source env.sh
-
-python3 render_configuration_template.py configuration.template.json > dags/configuration.json
-
-/app/entrypoint.sh
+if ! psql "${DATABASE_URL}" -c 'select 1'; then
+  echo waiting for DB &&\
+  while ! psql "${DATABASE_URL}" -c 'select 1'; do
+    echo . &&\
+    sleep 1
+  done
+fi &&\
+source env.sh &&\
+python3 render_configuration_template.py configuration.template.json > dags/configuration.json &&\
+exec /app/entrypoint.sh
